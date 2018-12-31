c01c6dea8b13eb7f4771d92ff8df858cedd3f865
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-31 20:56:01 -0300

    create actions by reducers (redux)

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 import styles from './styles/ListItemStyles';
+
 class ListItem extends Component {
 	render() {
 		const { library } = this.props;
 		const { title } = library;
 		const { titleStyle } = styles;
+
 		return (
 			<CardSection>
 				<Text style={titleStyle}>{title}</Text>
@@ -15,4 +19,7 @@ class ListItem extends Component {
 	}
 }
 
-export default ListItem;
+export default connect(
+	null,
+	actions
+)(ListItem);
